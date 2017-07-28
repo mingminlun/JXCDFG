20ae540fc8d1d93bf42c8df3703f69637da9ffc8
mingminlun <mingminlun@hotmail.com>
2017-07-28 11:23:05 +0800

    重叠覆盖统计修改

--- CDFG_count.py
+++ CDFG_count.py
@@ -5,3 +5,8 @@ import pandas as pd
 cdfg = pd.read_csv('C:\Work\JXWLJG\MR_07_222324\MRO-CDFG-20170723-all.csv',encoding = 'utf-8')
 del cdfg['\t']
 
+cdfg2 = cdfg[['CGI.1','重叠覆盖点数','大于-110采样点数']].dropna()
+
+grouped = cdfg2.groupby('CGI.1',as_index=False)
+
+
