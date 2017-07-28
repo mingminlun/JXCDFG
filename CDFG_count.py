28610720d3a05e92b2d31723181664f34ace5a81
mingminlun <mingminlun@hotmail.com>
2017-07-28 22:45:42 +0800

    修改2

--- CDFG_count.py
+++ CDFG_count.py
@@ -5,8 +5,7 @@ import pandas as pd
 cdfg = pd.read_csv('C:\Work\JXWLJG\MR_07_222324\MRO-CDFG-20170723-all.csv',encoding = 'utf-8')
 del cdfg['\t']
 
-cdfg2 = cdfg[['CGI.1','重叠覆盖点数','大于-110采样点数']].dropna()
+cdfg2 = cdfg[['CGI.1','重叠覆盖点数','大于-110采样点数']].dropna().groupby('CGI.1',as_index=False).sum()
 
-grouped = cdfg2.groupby('CGI.1',as_index=False)
-
+cdfg2['重叠覆盖度']=cdfg2['重叠覆盖点数']/cdfg2['大于-110采样点数']
 
